1e56ac72996b39a61131c2f553e749e314afa27f
Kirill Lagutin <lagutin.k@mail.ru>
2022-04-11 21:36:40 +0300

    update

--- TestingProgram.App/MainWindow.xaml.cs
+++ TestingProgram.App/MainWindow.xaml.cs
@@ -1,18 +1,70 @@
 ﻿using System.Security.Principal;
 using System.Windows;
+using System.Windows.Media;
 
 namespace TestingProgram.App
 {
     public partial class MainWindow : Window
     {
-        //публичное автосвойство из класса Account
-        //public Account Account { get; set; }
-        public MainWindow(/*Account account*/)
+        public MainWindow()
         {
             InitializeComponent();
 
-            //var role = Account.Roles.First().RoleId;
-            //TabControl_User.SelectedIndex = role - 1;
+            
+        }
+
+        private void Button_Reg_OnClick(object sender, RoutedEventArgs e)
+        {
+            string login = TextBox_Login.Text.Trim();
+            string password = PassBox_Pass.Password.Trim();
+            string password2 = PassBox_Pass2.Password.Trim();
+            string email = TextBox_Email.Text.Trim().ToLower();
+
+            if (login.Length < 3)
+            {
+                TextBox_Login.ToolTip = "Не менее 3-х символов!";
+                TextBox_Login.Background = Brushes.Red;
+            }
+            else
+            {
+                TextBox_Login.ToolTip = "";
+                TextBox_Login.Background = Brushes.Transparent;
+            }
+            
+            if (password.Length < 5)
+            {
+                PassBox_Pass.ToolTip = "Не менее 5-х символов!";
+                PassBox_Pass.Background = Brushes.Red;
+            }
+            else
+            {
+                PassBox_Pass.ToolTip = "";
+                PassBox_Pass.Background = Brushes.Transparent;
+            }
+            
+            if (password != password2)
+            {
+                PassBox_Pass2.ToolTip = "Пароли не совпадают!";
+                PassBox_Pass2.Background = Brushes.Red;
+            }
+            else
+            {
+                PassBox_Pass2.ToolTip = "";
+                PassBox_Pass2.Background = Brushes.Transparent;
+            }
+            
+            if (email.Length < 5 || !email.Contains("@") || !email.Contains("."))
+            {
+                TextBox_Email.ToolTip = "Некорректный Email!";
+                TextBox_Email.Background = Brushes.Red;
+            }
+            else
+            {
+                TextBox_Email.ToolTip = "";
+                TextBox_Email.Background = Brushes.Transparent;
+
+                MessageBox.Show("Вы зарегистрированы!");
+            }
         }
     }
 }
---
+++ TestingProgram.App/User.cs
@@ -0,0 +1,17 @@
+namespace TestingProgram.App
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string login { get; set; }
+        public string password { get; set; }
+        public string email { get; set; }
+
+        public User(string _login, string _password, string _email)
+        {
+            login = _login;
+            password = _password;
+            email = _email;
+        }
+    }
+}
