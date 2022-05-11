661a49f5e85e43f67d3699921e14891a7f58ca79
Pavel Popov <legeon48@mail.ru>
2022-05-11 21:39:38 +0300

    Changed

--- Models.lib/Subject.cs
+++ Models.lib/Subject.cs
@@ -4,5 +4,6 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public int Allotted_time { get; set; }
     }
 }
--- Models.lib/User.cs
+++ Models.lib/User.cs
@@ -1,6 +1,6 @@
 ﻿namespace Models.lib
 {
-    internal class User
+    public class User
     {
         public int Id { get; set; }
         public string FirstName { get; set; }
